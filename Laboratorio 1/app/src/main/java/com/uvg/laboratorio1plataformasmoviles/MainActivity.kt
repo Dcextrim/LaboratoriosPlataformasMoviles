21f774738939929a048808fcd4384711ea407a3c
Poposit <che231177@uvg.edu.gt>
2024-08-06 16:59:40 -0600

    Creacion de textos y botones

--- Laboratorio 1/app/src/main/java/com/uvg/laboratorio1plataformasmoviles/MainActivity.kt
+++ Laboratorio 1/app/src/main/java/com/uvg/laboratorio1plataformasmoviles/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.uvg.laboratorio1plataformasmoviles.ui.theme.Laboratorio1PlataformasMovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Laboratorio1PlataformasMovilesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +30,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MyScreen(modifier: Modifier = Modifier) {
+    var showMessage by remember { mutableStateOf(false) }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Plataformas Moviles")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Laboratorio 1 - Conociendo Android Studio")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Daniel Chet")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            showMessage = true
+            android.util.Log.d("MyScreen", "Button clicked")
+        }) {
+            Text(text = "Presionar")
+        }
+        if (showMessage) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Hola!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyScreenPreview() {
     Laboratorio1PlataformasMovilesTheme {
-        Greeting("Android")
+        MyScreen()
     }
 }
