9aeaf8fb9e537c780df34785cd493fa672b53308
Poposit <che231177@uvg.edu.gt>
2024-08-20 23:18:23 -0600

    Laboratorio 2 - Lemonade

---
+++ Laboratorio2/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lemonade.ui.theme.AppTheme
+//  actividad principal de la aplicación, heredando de ComponentActivity.
+//  Dentro de onCreate, se llama a setContent para definir el contenido
+//  de la interfaz de usuario usando Jetpack Compose.
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                LemonadeApp()
+            }
+        }
+    }
+}
+//unción Composable que define la estructura de la UI,
+// y AppTheme se usa para aplicar un tema visual.
+@OptIn(ExperimentalMaterial3Api::class)
+//Esta función Composable contiene la lógica principal de la aplicación.
+// Utiliza un Scaffold, que es un contenedor para la estructura básica de la aplicación,
+// incluyendo la barra superior (TopAppBar).
+@Composable
+fun LemonadeApp() {
+// Estado que maneja la etapa actual del proceso de hacer limonada.
+    var currentStep by remember { mutableStateOf(1) }
+//Estado que maneja cuántas veces se necesita apretar el limón.
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    // Dependiendo del valor de currentStep, la UI cambia
+    // para mostrar una imagen y un texto diferente, y manejar los clics de la imagen.
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentStep) {
+                1 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_select,
+                        drawableResourceId = R.drawable.lemon_tree,
+                        contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                        onImageClick = {
+                            currentStep = 2
+                            squeezeCount = (2..4).random()
+                        }
+                    )
+                }
+                2 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_squeeze,
+                        drawableResourceId = R.drawable.lemon_squeeze,
+                        contentDescriptionResourceId = R.string.lemon_content_description,
+                        onImageClick = {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                currentStep = 3
+                            }
+                        }
+                    )
+                }
+
+                3 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_drink,
+                        drawableResourceId = R.drawable.lemon_drink,
+                        contentDescriptionResourceId = R.string.lemonade_content_description,
+                        onImageClick = {
+                            currentStep = 4
+                        }
+                    )
+                }
+                4 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_empty_glass,
+                        drawableResourceId = R.drawable.lemon_restart,
+                        contentDescriptionResourceId = R.string.empty_glass_content_description,
+                        onImageClick = {
+                            currentStep = 1
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+// crea un diseño común para todas las etapas.
+// Muestra un botón con una imagen dentro, y un texto debajo de la imagen.
+@Composable
+fun LemonTextAndImage(
+    //textLabelResourceId, drawableResourceId, y contentDescriptionResourceId
+    // son recursos que se pasan para definir el texto, la imagen y la
+    // descripción para accesibilidad.
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // al hacer clic, ejecuta la función onImageClick. Dentro del botón,
+            // se muestra una imagen (Image) y un texto debajo (Text).
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+            ) {
+                Image(
+                    painter = painterResource(drawableResourceId),
+                    contentDescription = stringResource(contentDescriptionResourceId),
+                    modifier = Modifier
+                        .width(dimensionResource(R.dimen.button_image_width))
+                        .height(dimensionResource(R.dimen.button_image_height))
+                        .padding(dimensionResource(R.dimen.button_interior_padding))
+                )
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_vertical)))
+            Text(
+                text = stringResource(textLabelResourceId),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+//Proporciona una vista previa de la UI en el entorno de desarrollo (Android Studio).
+@Preview
+@Composable
+fun LemonPreview() {
+    AppTheme() {
+        LemonadeApp()
+    }
+}
